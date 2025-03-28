b5ad1aadc8e724e50fe952b26e0987d6a0ef9436
SofiaSVBD <sofii17227@gmail.com>
2025-03-27 23:53:58 -0300

    Add files via upload
    toma un valor limite ingresado por el usuario, y muestra por pantalla los números que son primos desde el 1 al limite indicado

---
+++ Numprimos.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+	int limite, i, j, primo;
+	
+	printf("Ingrese el numero limite de busqueda: \n");
+	scanf("%d", &limite);
+	
+	printf("Numeros primos desde 1 a %d: \n",limite);
+	
+	for (i = 1; i <= limite; i++) {
+		if (i < 2);
+		
+		primo=1;
+		
+		for(j =2; j*j<=i; j++){
+			if(i%j ==0) {
+				primo=0;
+				break;
+			}
+		}
+		if(primo) {
+			printf("%d ", i);
+		}
+	}
+	
+	printf("\n");
+	return 0;
+}
