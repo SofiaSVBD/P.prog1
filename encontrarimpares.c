b5c5a8ee6fc1d72feb4fe2bc14cbcc52e59bb32d
SofiaSVBD <sofii17227@gmail.com>
2025-04-25 14:14:22 -0300

    Add files via upload

---
+++ encontrarimpares.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+//Problema de hoy: Crear un programa que recorra un arreglo de enteros y al encontrar 
+//números impares, duplique su valor y muestre el nuevo arreglo con esos valores.  
+//Arreglo original: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,98,12]
+	
+int main() {
+	int arrayog[20]
+	
+	return 0;
+}
+
